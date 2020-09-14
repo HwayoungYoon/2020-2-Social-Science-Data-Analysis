75869dfdfef79ac659c5b23cb4368b9606deb880
Hwayoung Yoon <ghkdud204@gmail.com>
2020-09-14 20:58:22 +0900

    Update Ch10.R

--- Ch10.R
+++ Ch10.R
@@ -1,31 +1,31 @@
 setwd("C:/R/Social Science Data Analysis")
-### 데이터 불러들이기
+# 데이터 불러들이기
 load("spssdata.RData")
 
-### 데이터 만들기
+# 데이터 만들기
 myvar <- c("q33a01w1", "q33a02w1", "q33a03w1", "q33a04w1", "q33a05w1",
            "q33a06w1", "q33a07w1", "q33a08w1", "q33a09w1", "q33a10w1"  )
 PCA1   <- spssdata[myvar]
-# names(spssdata)
-# PCA1 <- spssdata[,c(9:18)]
+## names(spssdata)
+## PCA1 <- spssdata[,c(9:18)]
 PCA1_1 <- na.omit(PCA1) #결측값 제거
 
 #################### 요인분석 #####################
-### Factor의 수 결정 : 주성분분석
+# Factor의 수 결정 : 주성분분석
 fit1  <- princomp(PCA1_1, cor=TRUE)
 summary(fit1)
-plot(fit1, type="lines") #요인분석의 고유값 도표
-loadings(fit1) #요인들의 적재량
-#sum(loadings(fit1)[,1]^2)
+plot(fit1, type="lines") # 요인분석의 고유값 도표
+loadings(fit1) # 요인들의 적재량
+## sum(loadings(fit1)[,1]^2)
 
-### Factor에 따른 변수결정
+# Factor에 따른 변수결정
 ######### OPTION 1 #########
 fit <- factanal(PCA1_1, 2, rotation="varimax") 
-#요인수 2개의 요인분석 결과를 varimax 방법을 사용하여 회전
-print(fit, digits=3, sort=TRUE) #입력된 문항 순서대로 출력: sort=TRUE 옵션
-#summary(fit)
+# 요인수 2개의 요인분석 결과를 varimax 방법을 사용하여 회전
+print(fit, digits=3, sort=TRUE) # 입력된 문항 순서대로 출력: sort=TRUE 옵션
+## summary(fit)
 load <- fit$loadings[,1:2]
-plot(load) #도표출력
+plot(load) # 도표출력
 text(load, labels=names(PCA1_1), cex=0.7)
 
 ######### OPTION 2 ########
@@ -38,11 +38,11 @@ fit1_1
 # 데이터 쪼개기
 myvar1a <- c("q33a01w1", "q33a02w1", "q33a03w1", "q33a04w1", "q33a05w1", "q33a06w1")
 myvar1b <- c("q33a07w1", "q33a08w1", "q33a09w1", "q33a10w1")
-# myvar1a <- myvar[1:6] ; myvar1b <- myvar[7:10]
+## myvar1a <- myvar[1:6] ; myvar1b <- myvar[7:10]
 PCA1a <- spssdata[myvar1a]
 PCA1b <- spssdata[myvar1b]
 
-###신뢰도분석
+# 신뢰도분석
 library(psych)
 alpha(PCA1a, na.rm=TRUE)
 alpha(PCA1b, na.rm=TRUE)
@@ -56,12 +56,11 @@ tab_pca(PCA1, title="부모에 대한 애착", wrap.labels=20, show.cronb=TRUE,
 
 
 ##############################
-## 새로운 변수 생성
+# 새로운 변수 생성
 attach(spssdata)
 spssdata$gattach <- q33a01w1+q33a02w1+q33a03w1+q33a04w1+q33a05w1+q33a06w1
 spssdata$outatt  <- q33a07w1+q33a08w1+q33a09w1+q33a10w1
 detach(spssdata)
-
 
 
 ###########################################################################
@@ -75,5 +74,3 @@ newdata <- subset(test, scharew1 >= 100 & scharew1 < 200,
 library(sjPlot)
 tab_pca(newdata, title="자기만족도", wrap.labels=20, show.cronb=TRUE,
         show.var=TRUE, string.pov="분산비율", string.cpov="누적 비율")
-
-############################################################################
