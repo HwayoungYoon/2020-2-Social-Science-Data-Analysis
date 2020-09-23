b7bd8a6aaf337e98d0317bf269076fb2106d29a1
Hwayoung Yoon <ghkdud204@gmail.com>
2020-09-23 11:33:28 +0900

    Add files via upload

--- Ch02.R
+++ Ch02.R
@@ -11,6 +11,8 @@ test <- spss.get("[중2패널] 1차년도_6차년도 데이터(SPSS)/04-1 중2 패널 1차년도 �
                  use.value.labels=FALSE)
 ## names(test) : test 데이터셋에 저장된 변수들의 이름
 
+################################################################################
+
 # 변수 선택 - 숫자형 벡터 만들기
 select_variables <- c(1,4,9,10,19,101,102,103,328:337,339:348,372,375,377,379,
                       484:489, 496:504, 532)
@@ -49,34 +51,51 @@ labels.spss.values <- get_labels(spssdata1)
 # 관측값과 라벨 일치시킴(각 관측값들이 라벨 가짐)
 spssdata1 <- set_labels(spssdata, labels=labels.spss.values, force.values=FALSE, force.labels=TRUE)
 
+# 데이터 저장하기
+save(spssdata1, file="spssdata1.RData")
+
 ################################################################################
 
-# 변수생성
+# 변수생성(attachment, grade)
 attach(spssdata)
 spssdata$attachment = q33a01w1+q33a02w1+q33a03w1+q33a04w1+q33a05w1+q33a06w1
 #spssdata <- transform(spssdata, attachment=q33a01w1+q33a02w1+q33a03w1+q33a04w1+q33a05w1+q33a06w1)
 spssdata$grade = q18a1w1+q18a2w1+q18a3w1
 #detach(spssdata)
 
-# 학교성적 변수를 3집단으로 분류
+################################################################################
+
+# epiDisplay 패키지 install 및 load : 변수의 최댓값과 최솟값 사이의 분포를 알아보기 위함
 install.packages("epiDisplay")
 library(epiDisplay)
+
+# grade 변수에 대한 plot, table
+## cum.percent = TRUE : 누적 확률값 함께 출력
 tab1(spssdata$grade, cum.percent = TRUE)
+
+# 새로운 변수 attachment, grade를 불러들이기 위해 다시 한 번 attach
 attach(spssdata)
+
+# grade(학교성적) 변수를 3집단으로 분류
+## 조건 grade >= min(grade) & grade <= 8 를 만족하면 grp.grade에 1값 줌
 spssdata$grp.grade[grade >= min(grade) & grade <= 8] <- 1
 spssdata$grp.grade[grade >= 9 & grade <= 10] <- 2
 spssdata$grp.grade[grade >= 11 & grade <= max(grade)] <- 3
 #detach(spssdata)
+
+# grp.grade 변수에 대한 plot, table
 tab1(spssdata$grp.grade, cum.percent = TRUE)
 
-# q50w1의 속성을 부정/중립/긍정으로
+################################################################################
+
+# q50w1의 속성을 부정/중립/긍정으로 분류
 attach(spssdata)
-spssdata$satisfaction[q50w1==1|q50w1==2] <- 1 #만족하지 못하는 편
-spssdata$satisfaction[q50w1==3] <- 2          #보통
-spssdata$satisfaction[q50w1==4|q50w1==5] <- 3 #만족하는 편
+spssdata$satisfaction[q50w1==1|q50w1==2] <- 1 # 만족하지 못하는 편
+spssdata$satisfaction[q50w1==3] <- 2          # 보통
+spssdata$satisfaction[q50w1==4|q50w1==5] <- 3 # 만족하는 편
 #detach(spssdata)
 
-# 역방향 코딩
+# 역방향 코딩 : 응답의 방향성을 맞추기 위해
 attach(spssdata)
 spssdata$rq48a04w1[q48a04w1==1] <- 5
 spssdata$rq48a04w1[q48a04w1==2] <- 4
@@ -97,7 +116,7 @@ spssdata$rq48a06w1[q48a06w1==4] <- 2
 spssdata$rq48a06w1[q48a06w1==5] <- 1
 #detach(spssdata)
 
-# 두 변수의 변수값을 교차시켜 하나의 집단변수 만들기
+# 두 변수의 변수값(sexw1, grp.grade)을 교차시켜 하나의 집단변수 만들기
 #attach(spssdata)
 spssdata$grp.sex.grade[sexw1==1 & grp.grade==1] <- 11
 spssdata$grp.sex.grade[sexw1==1 & grp.grade==2] <- 12
@@ -107,10 +126,14 @@ spssdata$grp.sex.grade[sexw1==2 & grp.grade==2] <- 22
 spssdata$grp.sex.grade[sexw1==2 & grp.grade==3] <- 23
 #detach(spssdata)
 
-# 결측값은 NA로 지정
+# table 결과의 합이 전체 응답수와 다른 경우 결측값 존재
+# 결측값이 특정한 값으로 지정된 경우 NA로 변경
+## useNA="ifany" : 결측값의 갯수 확인
 q33a07w1
 table(q33a07w1)
 table(q33a07w1, useNA="ifany")
+
+################################################################################
 
 # 데이터 저장하기
 save(spssdata, file="spssdata.RData")
@@ -119,12 +142,14 @@ save(spssdata, file="spssdata.RData")
 save.image(file="myspssdata.RData")
 
 ################################################################################
+
 # 데이터 병합하기 
 second <- spss.get("[중2패널] 1차년도_6차년도 데이터(SPSS)/04-2 중2 패널 2차년도 데이터(SPSS).sav",
                  use.value.labels=FALSE)
 mergedata <- merge(spssdata, second, by="id")
 
 ################################################################################
+
 # 데이터 분할하기 
 newdata <- subset(test, scharew1 >= 100 & scharew1 < 200, 
                   select=c("id", "sexw1", "scharew1", "areaw1", "q2w1", "q18a1w1", 
